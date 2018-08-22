2bbcece8904931210d6f8b4a62ac3fe2561abed4
Thomas Haselwanter <thomas.haselwanter@rationalstorm.com>
2018-08-23 00:59:13 +0200

    Make CyberObservableCore abstract

--- stix2gson/src/main/java/eu/csaware/stix2/common/CyberObservableCore.java
+++ stix2gson/src/main/java/eu/csaware/stix2/common/CyberObservableCore.java
@@ -6,7 +6,7 @@ package eu.csaware.stix2.common;
  * <p>
  * Common properties and behavior across all Cyber Observable Objects.
  */
-public class CyberObservableCore {
+public abstract class CyberObservableCore {
 
     /**
      * Indicates that this object is an Observable Object. The value of this property MUST be a valid Observable Object type name, but to allow for custom objects this has been removed from the schema.
