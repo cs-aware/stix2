c8aaae1e2b4b3b2e224c3357e3b9a494bc05cc64
Thomas Haselwanter <thomas.haselwanter@rationalstorm.com>
2018-09-04 17:21:21 +0200

    Refactor Gson Singleton to parameterize it for custom class mappings and provide a second CS-Aware specific Singleton class

--- stix2gson/src/main/java/eu/csaware/stix2/util/Stix2Gson.java
+++ stix2gson/src/main/java/eu/csaware/stix2/util/Stix2Gson.java
@@ -8,24 +8,31 @@ import eu.csaware.stix2.common.TypedStixObject;
 import eu.csaware.stix2.extension.CSAwareVulnerability;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Provides Singletons for Gson instances. Based on enum rather than the Class-based Singleton pattern,
- * to solve multithreading issues without a locking solution. Provides a PRODUCTION instance which is intended
+ * Provides Singletons for Gson instances. Provides a PRODUCTION instance which is intended
  * for usage in live systems and a DEBUG instance which is intended for development and comes with pretty
  * printing to make development easier.
  */
-public enum Stix2Gson {
-    //prefix these with SINGLETON to make autocomplete pick up the PRODUCTION and DEBUG instances below directly
-    SINGLETON_PRODUCTION(commonGsonBuilder().create()),
-    SINGLETON_DEBUG(commonGsonBuilder().setPrettyPrinting().create());
+public class Stix2Gson {
 
-    private static GsonBuilder commonGsonBuilder() {
+    public static final Gson PRODUCTION = commonGsonBuilder().create();
+    public static final Gson DEBUG = commonGsonBuilder().setPrettyPrinting().create();
+
+    public static GsonBuilder commonGsonBuilder() {
+        return commonGsonBuilder(new HashMap<>());
+    }
+
+    public static GsonBuilder commonGsonBuilder(Map<Stix2Type, Class<? extends TypedStixObject>> customImplementations) {
         RuntimeTypeAdapterFactory<TypedStixObject> factory =
             RuntimeTypeAdapterFactory.of(TypedStixObject.class, "type");
 
-        //TODO provide way to register custom classes
-//        Stix2Type.VULNERABILITY.setImplementation(CSAwareVulnerability.class);
+        for (Stix2Type stix2Type : customImplementations.keySet()) {
+            stix2Type.setImplementation(customImplementations.get(stix2Type));
+        }
+
         for (Stix2Type value : Stix2Type.values()) {
             factory.registerSubtype(value.getImplementation(), value.toJsonString());
         }
@@ -34,25 +41,6 @@ public enum Stix2Gson {
             .disableHtmlEscaping() //without this patterns don't serialize correctly
             .registerTypeHierarchyAdapter(Collection.class, new EmptyCollectionNonSerializer())
             .registerTypeAdapterFactory(factory);
-    }
-
-    /**
-     * Convenience field to fetch the debug Gson instance directly.
-     */
-    public static final Gson DEBUG = SINGLETON_DEBUG.getGson();
-    /**
-     * Convenience field to fetch the production Gson instance directly.
-     */
-    public static final Gson PRODUCTION = SINGLETON_PRODUCTION.getGson();
-
-    private Gson gson;
-
-    Stix2Gson(Gson gson) {
-        this.gson = gson;
-    }
-
-    private Gson getGson() {
-        return gson;
     }
 
 }
